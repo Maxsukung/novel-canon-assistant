a27d322d4a705b5aba978ae6e29f73401d98da9d
Maxsukung <x7h2f9q6th@privaterelay.appleid.com>
2026-07-14 00:16:23 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='novel-studio-v9-character-database';
+const CACHE='novel-studio-v10-character-detail';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon-192.png','./icon-512.png','./mammoth.browser.min.js','./pdf.legacy.min.mjs','./pdf.legacy.worker.min.mjs'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
